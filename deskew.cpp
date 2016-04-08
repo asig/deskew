9b95e122dd69df01552e787825251d1f22d8a607
Andreas <andreas@fractal-labs.com>
2016-04-08 11:14:18 +0200

    transform to b/w, drop bad lines

--- deskew.cpp
+++ deskew.cpp
@@ -3,40 +3,56 @@
 
 using namespace std;
 
+const int RESIZED_HEIGHT = 1200;
+const int BW_THRESHOLD = 60;
+
+void showImage(const std::string& name, cv::Mat& img) {
+  cv::imshow(name, img);  
+  cv::waitKey(0);
+  cv::destroyWindow(name);
+}
+
 void compute_skew(const char* filename)
 {
   // Load in grayscale.
-  cv::Mat src = cv::imread(filename, 0);  
+  cv::Mat src = cv::imread(filename, 0);
 
   // resize image to something reasonable
   cv::Size size = src.size();
-  cv::Size new_size( ((float)size.width)/size.height*600, 600);
+  cv::Size new_size( ((float)size.width)/size.height*RESIZED_HEIGHT, RESIZED_HEIGHT);
   cv::Mat resized;
   cv::resize(src, resized, new_size);
 
+  // Convert it to white on black
   cv::bitwise_not(resized, resized);
+  resized = resized > BW_THRESHOLD;
 
-  cv::imshow(filename, resized);
-  cv::waitKey(0);
-     
+  showImage("Resized and b/w", resized);
+
+  // Compute lines     
   std::vector<cv::Vec4i> lines; 
-  cv::HoughLinesP(resized, lines, 1, CV_PI/180, 100, size.width / 50.f, 20);
-  // cv::HoughLinesP(src, lines, 1, CV_PI/180, 100, 40, 20);
-  cv::Mat disp_lines(new_size, CV_8UC1, cv::Scalar(0, 0, 0));
+  cv::HoughLinesP(resized, lines, 1, CV_PI/180, 100, size.width/50.f, 20); 
+  
+  cv::Mat disp_lines(new_size, CV_8UC3, cv::Scalar(0, 0, 0));
 
+  // Filter and show lines
   double total_angle = 0.;
   unsigned nb_lines = lines.size();
   unsigned int lines_used = 0;
+  const cv::Scalar RED = cv::Scalar(0, 0, 255);
+  const cv::Scalar GREEN = cv::Scalar(0, 255, 0);
   for (unsigned i = 0; i < nb_lines; ++i) {
         double angle = atan2((double)lines[i][3] - lines[i][1],
                     (double)lines[i][2] - lines[i][0]);
         double angle_deg = angle * 180 / CV_PI;
-        if (angle_deg > -20 && angle_deg < 20) {
-          cv::line(disp_lines, cv::Point(lines[i][0], lines[i][1]), cv::Point(lines[i][2], lines[i][3]), cv::Scalar(255, 0 ,0));
-
+        if (angle_deg > -15 && angle_deg < 15) {
+          cv::line(disp_lines, cv::Point(lines[i][0], lines[i][1]), cv::Point(lines[i][2], lines[i][3]), GREEN);
           total_angle += angle;
           lines_used++;
+        } else {
+          cv::line(disp_lines, cv::Point(lines[i][0], lines[i][1]), cv::Point(lines[i][2], lines[i][3]), RED);
         }
+
     }
     total_angle /= lines_used; // mean angle, in radians.
 
@@ -44,9 +60,7 @@ void compute_skew(const char* filename)
  
     std::cout << "File " << filename << ": " << total_angle * 180 / CV_PI << std::endl;
  
-    cv::imshow(filename, disp_lines);
-    cv::waitKey(0);
-    cv::destroyWindow(filename);
+    showImage("Lines", disp_lines);
 }
 
 void usage() {
